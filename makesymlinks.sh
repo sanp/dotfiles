30daef42e44fe3f054556104d9cb3c9b549f7ee3
Steve SanPietro <sanpietro.steve@gmail.com>
2013-09-17 12:56:20 -0500

    Added more files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,21 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="aliases gitconfig gitignore-global gitk gitmodules profile vim_mru_files viminfo vimrc"    # list of files/folders to symlink in homedir
+
+# files and folders which should start with a dot when symlinked
+# list of dot files to symlink in homedir
+dot_files="aliases profile gitconfig gitignore-global gitk gitmodules vim_mru_files viminfo vimrc"
+# list of dot folders to symlink in homedir
+dot_folders="vim"
+# all files/folders which start with a dot
+dot_links="$dot_files $dot_folders"
+
+# files and folders which should not start with a dot when symlinked
+# list of nondot files to symlink in homedir
+nondot_files="git-completion.bash"
+# list of nondot folders to symlink in homedir
+nondot_folders=
+nondot_links="$nondot_files $nondot_folders"
 
 ##########
 
@@ -23,9 +37,20 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
+# first do this for the dot_links
+for link in $dot_links; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    mv ~/.$link ~/dotfiles_old/
+    echo "Creating symlink to $link in home directory."
+    ln -s $dir/$link ~/.$link
+done
+
+# next do it for the nondot_links
+for link in $nondot_links; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    # note that the dot is not added before the link, as above
+    mv ~/$link ~/dotfiles_old/
+    echo "Creating symlink to $link in home directory."
+    # again, no dot is added
+    ln -s $dir/$link ~/$link
 done
