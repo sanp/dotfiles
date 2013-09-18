c4bd37ddfff6146daaec51ab3a33bf5d7817cf4f
Steve SanPietro <sanpietro.steve@gmail.com>
2013-09-17 23:25:28 -0500

    Put symlink vars in their own file

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,11 @@
 ############################################################################
 
 cd ~
-rm -rf .aliases .gitconfig .gitignore-global .gitk .profile .vim \
-    .vim_mru_files .viminfo .vimrc git-completion.bash vimbackup vimtmpdir \
-    dotfiles dotfiles_old
+
+# Rather than hard code the names of the files to remove, just source the
+# makesymlinks script
+source ~/dotfiles/files_and_folders.sh
+
+dotfiles="$dot_links $nondot_links"
+
+rm -rf $dotfiles
