7d47cca5fd8a24e0743015ca287bd4dd83b5228c
Steve SanPietro <sanpietro.steve@gmail.com>
2013-09-17 23:25:28 -0500

    Put symlink vars in their own file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,20 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
-# files and folders which should start with a dot when symlinked
-# list of dot files to symlink in homedir
-dot_files="aliases profile gitconfig gitignore-global gitk vim_mru_files viminfo vimrc"
-# list of dot folders to symlink in homedir
-dot_folders="vim"
-# all files/folders which start with a dot
-dot_links="$dot_files $dot_folders"
-
-# files and folders which should not start with a dot when symlinked
-# list of nondot files to symlink in homedir
-nondot_files="git-completion.bash"
-# list of nondot folders to symlink in homedir
-nondot_folders="vimtmpdir vimbackup"
-nondot_links="$nondot_files $nondot_folders"
+source ./files_and_folders.sh
 
 ##########
 
