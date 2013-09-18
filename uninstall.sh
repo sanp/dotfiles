9f7aaa958adca7b5c08bf0d05e2aa61b5c76e9f8
Steve SanPietro <sanpietro.steve@gmail.com>
2013-09-17 23:51:16 -0500

    Changed uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,33 @@
 # This script uninstalls all dotfiles, giving you a clean home folder. 
 ############################################################################
 
-cd ~
+# This function deletes all dotfiles and folders
+function uninstall {
 
-# Rather than hard code the names of the files to remove, just source the
-# makesymlinks script
-source ~/dotfiles/files_and_folders.sh
+    # Rather than hard code the names of the files to remove, just source the
+    # makesymlinks script
+    source ~/dotfiles/files_and_folders.sh
 
-dotfiles="$dot_links $nondot_links"
+    dotfiles=""
 
-rm -rf $dotfiles
+    # Add the files which start with a dot to the dotfiles var
+    for link in $dot_links; do
+        dotfiles="$dotfiles ~/.$link"
+    done
+
+    # Add the files which do not start with a dot to the dotfiles var
+    for link in $nondot_links; do
+        dotfiles="$dotfiles ~/$link"
+    done
+
+    rm -rf $dotfiles
+
+}
+
+# Make sure the user is sure
+read -n1 -p "Are you sure? [y,n]" doit 
+case $doit in  
+  y|Y) uninstall ;; 
+  n|N) echo Phew! That was close... ;; 
+  *) echo Invalid answer;; 
+esac
