05bed81e14efde2bec70743c0e4275e85208ce92
Steve SanPietro <steven.sanpietro@rate.com>
2020-06-05 23:57:13 -0500

    Added iterm and karabiner configs and removed things from manual.md

--- install.sh
+++ install.sh
@@ -55,6 +55,7 @@ chsh -s /usr/local/bin/zsh
 
 # Oh my zsh
 ZSH_CUSTOM=~/.oh-my-zsh/custom
+rm -rf ${ZSH_CUSTOM}
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
